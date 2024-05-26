5cf35795f25aba11f9414f0b61fe53b6eb1bbea6
Quang Thang <thang@thangqt.com>
2024-05-27 00:10:24 +0700

    Limit generation to 5 on boot menu

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
 
   networking.hostName = "matrix";
 
