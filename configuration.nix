3cc80299e619197197d0a440779f0c5e91498c42
Quang Thang <thang@thangqt.com>
2024-05-27 10:36:43 +0700

    Increase generatation on boot menu to 15

--- configuration.nix
+++ configuration.nix
@@ -12,7 +12,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 5;
+  boot.loader.systemd-boot.configurationLimit = 15;
 
   networking.hostName = "matrix";
 
