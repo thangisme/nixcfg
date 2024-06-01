bb2675a8afc73c0d534014964605be298210876a
Quang Thang <thang@thangqt.com>
2024-06-01 13:15:51 +0700

    Upgrade to nix 24.05

--- flake.nix
+++ flake.nix
@@ -3,10 +3,10 @@
 
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
