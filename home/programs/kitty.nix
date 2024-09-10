e9af24802abc9f03269e91bb5e759c8743fdd8c6
Quang Thang <thang@thangqt.com>
2024-09-10 15:34:23 +0700

    format  file

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -1,14 +1,15 @@
-{ config, pkgs, ... } : {
-    programs.kitty = {
-        enable = true;
-        theme = "Catppuccin-Mocha";
-        font.name = "IosevkaTerm Nerd Font";
-        shellIntegration = {
-          enableFishIntegration = true;
-          mode = "no-cursor";
-        };
-        extraConfig = ''
-          window_padding_width 4 8
-        '';
+{ config, pkgs, ... }:
+{
+  programs.kitty = {
+    enable = true;
+    theme = "Catppuccin-Mocha";
+    font.name = "IosevkaTerm Nerd Font";
+    shellIntegration = {
+      enableFishIntegration = true;
+      mode = "no-cursor";
     };
+    extraConfig = ''
+      window_padding_width 4 8
+    '';
+  };
 }
