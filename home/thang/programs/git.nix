c8f5f7cb732e88ac969b6538861ccfc5a365483f
Quang Thang <thang@thangqt.com>
2024-09-26 23:04:50 +0700

    Git libsecret support

--- home/thang/programs/git.nix
+++ home/thang/programs/git.nix
@@ -1,7 +1,12 @@
-{ config, ... }: {
+{ pkgs, ... }:
+{
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     userName = "Quang Thang";
     userEmail = "thang@thangqt.com";
+    extraConfig = {
+      credential.helper = "libsecret";
+    };
   };
 }
