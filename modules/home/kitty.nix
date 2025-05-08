07f30679d8021794f56f38598316230b61ee4b70
Quang Thang <thang@thangqt.com>
2025-05-08 19:01:13 +0700

    updated kitty cfg

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -11,6 +11,10 @@
     extraConfig = ''
       window_padding_width 4 8
       hide_window_decorations true
+      tab_bar_style powerline
+      tab_powerline_style slanted
+      window_margin_width 1
+      single_window_margin_width 0
     '';
   };
 }
